7e32d10a2475829d6997f720ef631af3136763f4
Taj <betetat94@gmail.com>
2022-06-12 23:32:46 -0400

    pull current weather data and display on application

--- assets/script.js
+++ assets/script.js
@@ -12,28 +12,105 @@ console.log(geoCode);
 var getGeoCode = function () {
     var response = fetch(geoCode).then(function (response) {
         response.json().then(function (data) {
-            var geoData = data;
-            console.log(geoData);
-            console.log(geoData[0]);
-            console.log(geoData[0].lat);
-            console.log(geoData[0].lon);
+            var locationData = data;
+            console.log(locationData);
+            console.log(locationData[0]);
+            console.log(locationData[0].lat);
+            console.log(locationData[0].lon);
+            console.log(locationData[0].name);
 
-            var lat = geoData[0].lat;
-            var lon = geoData[0].lon;
+            var lat = locationData[0].lat;
+            var lon = locationData[0].lon;
+            var city = locationData[0].name;
 
-            var newData = {
+
+
+            var geoData = {
                 "latitude": lat,
-                "longitude": lon
+                "longitude": lon,
+                "city": city
             }
-            console.log(newData);
 
-            getWeather(newData);
+            console.log(geoData);
+
+            geoArr.push(geoData);
+
+            localStorage.setItem("coordinates", geoArr);
+
+            getCurrentWeather(geoData);
+
+
+            // var cityDisplay = document.getElementById("city").innerHTML = city;
+
+            // var badgeArea = document.getElementById("showbadge");
+            // var cityBadge = document.createElement("p").classList;
+            // cityBadge.add("badge", "rounded-pill", "bg-secondary", "text-white", "col-12");
+            // console.log(cityBadge);
+            // var cityBadgeText = document.createTextNode(city);
+            // console.log(cityBadgeText);
+            // cityBadge.appendChild(cityBadgeText);
+            // badgeArea.appendChild(cityBadge);
         });
     });
 }
 
-var getWeather = function () {
+var geoArr = [];
+
+
+var getCurrentWeather = function (geoArr) {
+    console.log(geoArr);
+    var string1 = "https://api.openweathermap.org/data/2.5/onecall?";
+    var string2 = "lat=" + geoArr.latitude + "&" + "lon=" + geoArr.longitude + apiKey;
+    var weatherSearch = string1.concat(string2);
+    console.log(weatherSearch);
+
+    var weatherResponse = fetch(weatherSearch).then(function (weatherResponse) {
+        weatherResponse.json().then(function (data) {
+            console.log("weather data", data);
+            var currentDayWeather = data;
+            var currentTemp = currentDayWeather.current.temp;
+            var currentHumidity = currentDayWeather.current.humidity;
+            var currentUvi = currentDayWeather.current.uvi;
+            var currentWind = currentDayWeather.current.wind_speed;
+
+            var charlotteTemp = document.getElementById("temp").innerHTML = currentTemp;
+            var charlotteWind = document.getElementById("wind").innerHTML = currentWind;
+            var charlotteHumidity = document.getElementById("humidity").innerHTML = currentHumidity;
+            var charlotteUvi = document.getElementById("uvi").innerHTML = currentUvi;
+
+        });
+    });
 
 }
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+console.log(today);
+
+document.getElementById("date").innerHTML = today;
+// document.write(today);
+
+// console.log(newArr);
+
+// var lat = geoData[0].lat;
+// var lon = geoData[0].lon;
+// var text3 = "https://api.openweathermap.org/data/2.5/onecall?";
+// var text4 = lat + "&" + lon;
+
+// var weatherSearch = text3.concat(text4);
+
+// console.log(weatherSearch);
+
+
+// var text3 = "https://api.openweathermap.org/data/2.5/onecall?lat=";
+
+// 35.2272086 & lon=-80.8430827 & appid=d68b4bb3b1bf58ab1bbd62b4fa0d5699
+
+
 getGeoCode();
+
+getCurrentWeather();
