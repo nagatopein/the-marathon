7ac971a17f9fda3f775ce664706afe58eb2c0940
Taj <betetat94@gmail.com>
2022-06-27 01:36:59 -0400

    major refractor, added ui search

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,89 @@
-var apiKey = "&appid=d68b4bb3b1bf58ab1bbd62b4fa0d5699"
-var cityName = "charlotte";
-var stateCode = "nc";
-var countryCode = "us";
+var apiKey = "appid=d68b4bb3b1bf58ab1bbd62b4fa0d5699"
 
-var text1 = "http://api.openweathermap.org/geo/1.0/direct?q=";
-var text2 = cityName + "," + stateCode + "," + countryCode + apiKey;
-var geoCode = text1.concat(text2);
 
-console.log(geoCode);
+var geoArr = [];
 
-var getGeoCode = function () {
-    var response = fetch(geoCode).then(function (response) {
-        response.json().then(function (data) {
-            var locationData = data;
-            console.log(locationData);
-            console.log(locationData[0]);
-            console.log(locationData[0].lat);
-            console.log(locationData[0].lon);
-            console.log(locationData[0].name);
+// var cityArr = [];
 
+var citySearch = document.getElementById("searchBtn");
+
+// get value of search-bar and intitate first search
+citySearch.addEventListener('click', function () {
+    var searchParameter = document.getElementById("citySearch").value;
+
+    console.log(searchParameter);
+
+
+    var geoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${searchParameter},US&${apiKey}`;
+    getGeoCode(geoCode);
+});
+
+var getGeoCode = async function (geoCode) {
+    var response = await fetch(geoCode).then(function (response) {
+        response.json().then(function (locationData) {
             var lat = locationData[0].lat;
             var lon = locationData[0].lon;
             var city = locationData[0].name;
 
 
 
-            var geoData = {
-                "latitude": lat,
-                "longitude": lon,
+            var geoObj = {
+                "lat": lat,
+                "lon": lon,
                 "city": city
             }
 
-            console.log(geoData);
+            // console.log(geoObj);
 
-            geoArr.push(geoData);
+            geoArr.push(geoObj);
 
-            localStorage.setItem("coordinates", geoArr);
+            // console.log(geoArr);
 
-            getCurrentWeather(geoData);
+            localStorage.setItem("coordinates", JSON.stringify(geoArr));
 
+            document.getElementById("city").innerHTML = city;
 
-            var cityDisplay = document.getElementById("city").innerHTML = city;
-
-            // var badgeArea = document.getElementById("showbadge");
-            // var cityBadge = document.createElement("p").classList;
-            // cityBadge.add("badge", "rounded-pill", "bg-secondary", "text-white", "col-12");
-            // console.log(cityBadge);
-            // var cityBadgeText = document.createTextNode(city);
-            // console.log(cityBadgeText);
-            // cityBadge.appendChild(cityBadgeText);
-            // badgeArea.appendChild(cityBadge);
+            getCurrentWeather(geoObj);
         });
     });
 }
 
-var geoArr = [];
+var badgeArea = document.getElementById("searchHistory");
 
+var getCurrentWeather = async function () {
+    var getGeoData = JSON.parse(localStorage.getItem("coordinates"));
+    // console.log(getGeoData);
+    var lat = getGeoData[0].lat;
+    var lon = getGeoData[0].lon;
 
-var getCurrentWeather = function (geoArr) {
-    console.log(geoArr);
-    var string1 = "https://api.openweathermap.org/data/2.5/onecall?";
-    var string2 = "lat=" + geoArr.latitude + "&" + "lon=" + geoArr.longitude + apiKey;
-    var weatherSearch = string1.concat(string2);
-    console.log(weatherSearch);
+    var weatherSearch = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&${apiKey}`;
+    // console.log(weatherSearch);
 
-    var weatherResponse = fetch(weatherSearch).then(function (weatherResponse) {
+    // weatherResponse(weatherSearch);
+    var weatherResponse = await fetch(weatherSearch).then(function (weatherResponse) {
         weatherResponse.json().then(function (data) {
-            console.log("weather data", data);
+            // console.log("weather data", data);
             var currentDayWeather = data;
-            var currentTemp = currentDayWeather.current.temp;
-            var currentHumidity = currentDayWeather.current.humidity;
+            console.log(currentDayWeather);
+            var currentTemp = currentDayWeather.current.temp + " °F";
+            var currentHumidity = currentDayWeather.current.humidity + " %";
             var currentUvi = currentDayWeather.current.uvi;
-            var currentWind = currentDayWeather.current.wind_speed;
+            var currentWind = currentDayWeather.current.wind_speed + " MPH";
 
             var charlotteTemp = document.getElementById("temp").innerHTML = currentTemp;
             var charlotteWind = document.getElementById("wind").innerHTML = currentWind;
             var charlotteHumidity = document.getElementById("humidity").innerHTML = currentHumidity;
             var charlotteUvi = document.getElementById("uvi").innerHTML = currentUvi;
 
+            fiveDayForecast(data);
+
         });
     });
+};
 
+var fiveDayForecast = function (forecastData) {
+    var week = forecastData.daily.slice(0, 5);
+    console.log(week);
 }
 
 var today = new Date();
@@ -92,25 +95,121 @@ today = mm + '/' + dd + '/' + yyyy;
 console.log(today);
 
 document.getElementById("date").innerHTML = today;
-// document.write(today);
-
-// console.log(newArr);
-
-// var lat = geoData[0].lat;
-// var lon = geoData[0].lon;
-// var text3 = "https://api.openweathermap.org/data/2.5/onecall?";
-// var text4 = lat + "&" + lon;
-
-// var weatherSearch = text3.concat(text4);
-
-// console.log(weatherSearch);
 
 
-// var text3 = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
-// 35.2272086 & lon=-80.8430827 & appid=d68b4bb3b1bf58ab1bbd62b4fa0d5699
+// citySearchHistory();
+
+// getGeoCode();
+
+// getCurrentWeather();
+
+// fiveDayForecast();
+
+// addSearchHistory();
 
 
-getGeoCode();
 
-getCurrentWeather();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cityName = "charlotte";
+// var stateCode = "nc";
+// var countryCode = "us";
+
+
+
+
+
+// .onclick = function () {
+//     var citySearch = document.getElementById("citySearch").value;
+//     console.log("search", citySearch);
+//     // var searchBtn = document.getElementById("search");
+//     // searchBtn.addEventListener('click',)
+
+
+//     // console.log(geoCode);
+//     getGeoCode(geoCode);
+
+// }
+
+
+
+
+
+
+            // cityBadge.add("badge", "rounded-pill", "bg-secondary", "text-white", "col-12");
+            // console.log(cityBadge);
+            // var cityBadgeText = document.createTextNode(city);
+            // console.log(cityBadgeText);
+            // cityBadge.appendChild(cityBadgeText);
+            // badgeArea.appendChild(cityBadge);
+
+
+
+
+
+
+
+
+
+
+// var addSearchHistory = function () {
+//     var cityArr = JSON.parse(localStorage.getItem('coordinates'));
+//     console.log(cityArr);
+//     for (i = 0; i < cityArr.length; i++) {
+//         var cityName = cityArr[i].city;
+
+//         var cityBadge = document.createElement("p");
+//         cityBadge.setAttribute("class", "badge rounded-pill bg-secondary text-white col-12");
+//         cityBadge.innerHTML = cityName;
+//         badgeArea.appendChild(cityBadge);
+//     }
+// }
